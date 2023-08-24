705f5fd5d4ebc066ca58946025ae27f21d90b723
Paulina Tomaszewska <paulina.tomaszewska111@gmail.com>
2023-08-24 11:19:39 +0200

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,15 @@
 apt-get -y install libopenjp2-7-dev libopenjp2-tools openslide-tools
 conda create -n PCam_context python=3.8 -y
 conda activate PCam_context
-pip install -r requirements.txt
+
+conda install pytorch==1.7.1 torchvision torchaudio cudatoolkit=11.0 -c pytorch
+conda install pandas matplotlib seaborn scikit-learn scipy
+
+pip install h5py
+pip install torchmetrics
+pip install --upgrade torchaudio
+
+pip install ml_collections
+pip install timm
+pip install torch
+conda install -c conda-forge pixman
