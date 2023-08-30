ffe4c7d2d390eec3f35b6a05699d7fa4b31791dc
MattS0000 <56126527+MattS0000@users.noreply.github.com>
2023-08-30 11:11:43 +0200

    Update run_all_pretrained.sh

--- run_all_pretrained.sh
+++ run_all_pretrained.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir pretrained_experiments
-for value in pcamswin pcammoco pcamsup pcammae; do
+for value in pcamswin pcammoco pcamsup pcammae resnet18-pcam densenet121-pcam; do
    sbatch run_single.sh $value
 done
